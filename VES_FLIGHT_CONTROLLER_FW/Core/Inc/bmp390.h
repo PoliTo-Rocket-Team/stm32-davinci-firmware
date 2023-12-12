61aeac974245f0f0a30725d4b76428ea86680e03
Tommaso Gualtierotti <gualtierotti.tommaso@gmail.com>
2023-12-12 17:17:06 +0100

    feat: add functions prototype to use bmp390 sensor

--- VES_FLIGHT_CONTROLLER_FW/Core/Inc/bmp390.h
+++ VES_FLIGHT_CONTROLLER_FW/Core/Inc/bmp390.h
@@ -14,7 +14,34 @@
 extern "C" {
 #endif
 
-//UINT8 var = BMP390_MODE_SLEEP;
+/*
+ * @details This API is the entry point.
+ *
+ * @param[in, out] handler : structure instance of bmp390_handler
+ * @retval Result of API execution status
+ * @retval 0	-> Success
+ * @retval > 0	-> Warning
+ * @retval < 0	-> Error
+ */
+INT8 bmp390_INIT(struct bmp390_handler *handler);
+
+INT8 bmp390_soft_reset(struct bmp390_handler *handler);
+
+INT8 bmp390_set_sensor_settings(INT32 desired_settings, struct bmp390_settings *settings, struct bmp390_handler *handler);
+
+INT8 bmp390_get_sensor_settings(struct bmp390_settings *settings, struct bmp390_handler *handler);
+
+INT8 bmp390_set_operating_mode(struct bmp390_settings *settings, struct bmp390_handler *handler);
+
+INT8 bmp390_get_operating_mode(UINT8 *op_mode, struct bmp390_handler *handler);
+
+INT8 bmp390_get_sensor_data(UINT8 sensor_comp, struct bmp390_data *data, struct bmp390_handler *handler);
+
+INT8 bmp390_set_registers(UINT8 *reg_address, const UINT8 *reg_data, UINT32 len, struct bmp390_handler *handler);
+
+INT8 bmp390_get_registers(UINT8 *reg_address, UINT8 *reg_data, UINT32 len, struct bmp390_handler *handler);
+
+INT8 bmp390_get_status(struct bmp390_status *status, struct bmp390_handler *handler);
 
 #ifdef __cplusplus
 }
