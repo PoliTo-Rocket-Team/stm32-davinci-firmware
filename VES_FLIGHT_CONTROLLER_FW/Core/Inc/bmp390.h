94045faa61b0020ccd1b9bf948c060ff3381603f
Tommaso Gualtierotti <gualtierotti.tommaso@gmail.com>
2023-12-12 19:15:52 +0100

    fix: fix some error on parameters type in bmp390 get and set registers function

--- VES_FLIGHT_CONTROLLER_FW/Core/Inc/bmp390.h
+++ VES_FLIGHT_CONTROLLER_FW/Core/Inc/bmp390.h
@@ -35,11 +35,11 @@ INT8 bmp390_set_operating_mode(struct bmp390_settings *settings, struct bmp390_h
 
 INT8 bmp390_get_operating_mode(UINT8 *op_mode, struct bmp390_handler *handler);
 
-INT8 bmp390_get_sensor_data(UINT8 sensor_comp, struct bmp390_data *data, struct bmp390_handler *handler);
+INT8 bmp390_get_sensor_data(UINT8 sensor_comp, struct bmp390_compensated_sensor_data *data, struct bmp390_handler *handler);
 
-INT8 bmp390_set_registers(UINT8 *reg_address, const UINT8 *reg_data, UINT32 len, struct bmp390_handler *handler);
+INT8 bmp390_set_registers(UINT8 reg_address, const UINT8 *reg_data, UINT32 len, struct bmp390_handler *handler);
 
-INT8 bmp390_get_registers(UINT8 *reg_address, UINT8 *reg_data, UINT32 len, struct bmp390_handler *handler);
+INT8 bmp390_get_registers(UINT8 reg_address, UINT8 *reg_data, UINT32 len, struct bmp390_handler *handler);
 
 INT8 bmp390_get_status(struct bmp390_status *status, struct bmp390_handler *handler);
 
