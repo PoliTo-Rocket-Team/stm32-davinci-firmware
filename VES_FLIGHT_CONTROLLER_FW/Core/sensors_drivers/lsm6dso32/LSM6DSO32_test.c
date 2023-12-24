821d31d0619d31bfcc5475934f2b952efd49af24
tommy-tg <gualtierotti.tommaso@gmail.com>
2023-12-24 22:25:17 +0100

    chore: add LSM6DSO32_test.c file

---
+++ VES_FLIGHT_CONTROLLER_FW/Core/sensors_drivers/lsm6dso32/LSM6DSO32_test.c
@@ -0,0 +1,20 @@
+/*
+ * LSM6DSO32_test.c
+ *
+ *  Created on: Dec 24, 2023
+ *      Author: tomma
+ */
+
+#ifndef LSM6DSO32_REGS_H
+#define LSM6DSO32_REGS_H
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
