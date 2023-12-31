44bb53ae06ad5d7bcdce3d6632ba9c703bdeadad
tommy-tg <gualtierotti.tommaso@gmail.com>
2023-12-31 17:49:10 +0100

    chore: add utils files
    - add utils.h and utils.c which now contain the definition and
      declaration of COM_port_serial_print function previously defined in
      main.h and main.c files.

---
+++ VES_FLIGHT_CONTROLLER_FW/Core/sensors_drivers/utils.c
@@ -0,0 +1,17 @@
+/*
+ * utils.c
+ *
+ *  Created on: Dec 31, 2023
+ *      Author: tomma
+ */
+
+#include "utils.h"
+
+#ifdef DEBUG_EN
+
+#include <string.h>
+
+HAL_StatusTypeDef COM_port_serial_print(const uint8_t* data) {
+	return HAL_UART_Transmit(&huart2, data, strlen((const char *)data) + 1, 100);
+}
+#endif
---
+++ VES_FLIGHT_CONTROLLER_FW/Core/sensors_drivers/utils.h
@@ -0,0 +1,17 @@
+/*
+ * utils.h
+ *
+ *  Created on: Dec 31, 2023
+ *      Author: tomma
+ */
+
+#ifndef SENSORS_DRIVERS_UTILS_H_
+#define SENSORS_DRIVERS_UTILS_H_
+
+#define DEBUG_EN
+
+#ifdef DEBUG_EN
+HAL_StatusTypeDef COM_port_serial_print(const uint8_t* data);
+#endif
+
+#endif /* SENSORS_DRIVERS_UTILS_H_ */
