cbbb691882bbf8af5179c484ebae2998abd6ce61
Tommaso Gualtierotti <gualtierotti.tommaso@gmail.com>
2024-03-17 22:45:50 +0100

    chore: remove code to test flash from main
    - code to test flash moved to test.c test_w25q128 function.

--- VES_FLIGHT_CONTROLLER_FW/Core/Inc/main.h
+++ VES_FLIGHT_CONTROLLER_FW/Core/Inc/main.h
@@ -33,6 +33,7 @@ extern "C" {
 /* USER CODE BEGIN Includes */
 #include "W25Q128.h"
 #include "servo.h"
+#include "test.h"
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
