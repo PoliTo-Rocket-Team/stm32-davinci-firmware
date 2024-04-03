4a1030b74af8df3877f479e6e9c30414a97125f0
Tommaso Gualtierotti <gualtierotti.tommaso@gmail.com>
2024-04-03 21:32:33 +0200

    chore: add header and source files for buzzer handling

---
+++ VES_FLIGHT_CONTROLLER_FW/Core/Inc/buzzer.h
@@ -0,0 +1,13 @@
+/*
+ * buzzer.h
+ *
+ *  Created on: Apr 3, 2024
+ *      Author: tommaso
+ */
+
+#ifndef BUZZER_H
+#define BUZZER_H
+
+
+
+#endif /* INC_BUZZER_H_ */
---
+++ VES_FLIGHT_CONTROLLER_FW/Core/Src/buzzer.c
@@ -0,0 +1,10 @@
+/*
+ * buzzer.c
+ *
+ *  Created on: Apr 3, 2024
+ *      Author: tommaso
+ */
+
+#ifndef BUZZER_H
+#include "buzzer.h"
+#endif
