f114a349617aaedf0d0a8269cecf967b8dec32a2
Tommaso Gualtierotti <gualtierotti.tommaso@gmail.com>
2024-04-04 12:30:10 +0200

    feat: add functions for buzzer handling, not tested yet
    - still missing the implementation for the note lookup to select the correct frequency
    - still to be tested with hardware

--- VES_FLIGHT_CONTROLLER_FW/Core/Inc/buzzer.h
+++ VES_FLIGHT_CONTROLLER_FW/Core/Inc/buzzer.h
@@ -8,6 +8,42 @@
 #ifndef BUZZER_H
 #define BUZZER_H
 
+#include "main.h"
 
+#define A4 	400
+#define Bb4	466
+#define B4	494
+#define C5	523
+#define Db5	554
+#define D5	587
+#define Eb5	622
+#define E5	659
+#define F5	699
+#define Gb5	740
+#define G5	784
+#define Ab5	831
+#define A5	880
+#define Bb5	932
+#define B5	988
+#define C6	1047
+
+extern TIM_HandleTypeDef htim4;
+#define TIMER4_CLK	96000000
+#define TIM_CHANNEL TIM_CHANNEL_4
+
+typedef struct {
+
+	volatile uint32_t *TIM_CCR;
+	TIM_TypeDef *TIM_Instance;
+
+} buzzer_t;
+
+void buzzerInit(buzzer_t *dev);
+
+void setVolume(buzzer_t *dev, uint8_t volume);
+
+void setNote(char* note);
+
+void beepBuzzer(uint32_t duration);
 
 #endif /* INC_BUZZER_H_ */
--- VES_FLIGHT_CONTROLLER_FW/Core/Src/buzzer.c
+++ VES_FLIGHT_CONTROLLER_FW/Core/Src/buzzer.c
@@ -8,3 +8,40 @@
 #ifndef BUZZER_H
 #include "buzzer.h"
 #endif
+
+static void setFrequency(uint16_t frequency) {
+
+	uint16_t prescaler = 0;
+
+	prescaler = ((TIMER4_CLK / htim4.Init.Period) / frequency) - 1;
+
+	htim4.Init.Prescaler = prescaler;
+}
+
+void buzzerInit(buzzer_t *dev) {
+
+	dev -> TIM_CCR = &(TIM4 -> CCR4);
+	dev -> TIM_Instance = TIM4;
+	htim4.Init.Period = 200;
+}
+
+void setVolume(buzzer_t *dev, uint8_t volume) {
+
+	*(dev -> TIM_CCR) = volume < 100 ? volume : 100;
+}
+
+void setNote(char* note) {
+
+	uint16_t freq = 0;
+
+	//lookup to the correct frequency from the note parameter
+
+	setFrequency(freq);
+}
+
+void beepBuzzer(uint32_t duration) {
+
+	HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL);
+	osDelay(duration);
+	HAL_TIM_PWM_Stop(&htim4, TIM_CHANNEL);
+}
